b1d8352b0ed98a1e1e66ed1d4e403c639d3765e7
taktez <v.keshavarz9@gmail.com>
2024-10-10 10:33:23 +0330

    make search add home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant/constant/constant.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,10 +11,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text("HomePage"),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                width: size.width * 0.9,
+                decoration: BoxDecoration(
+                  color: Constant.primaryColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.search),
+                    SizedBox(width: 5.0),
+                    Expanded(
+                        child: TextField(
+                      style: TextStyle(fontFamily: 'YekanBakh'),
+                      decoration: InputDecoration(
+                          hintText: 'جستجو...',
+                          border: InputBorder.none,
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 12)),
+                    )),
+                    Icon(Icons.mic),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
